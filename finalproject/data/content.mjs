88dcc695391ff9928cffbabcc4acf7734e01ec4d
daniel257p <dpaulinopolanco@byupathway.edu>
2026-02-13 11:57:03 -0600

    fixed some repeated selectors and change the larger images size

--- finalproject/data/content.mjs
+++ finalproject/data/content.mjs
@@ -2,17 +2,14 @@ export const featuresData = [
     {
         title: "Learn the Basics",
         description: "Simple explanations for beginners.",
-        image: "images/background1.jpg"
     },
     {
         title: "Explore Projects",
         description: "Browse major DePIN networks.",
-        image: "images/background1.jpg"
     },
     {
         title: "Track Market Data",
         description: "View token prices and market caps.",
-        image: "images/background1.jpg"
     }
 ];
 
